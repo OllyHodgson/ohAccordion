cc1e96546f8486dfd6734c368bd0f450574bca15
Olly Hodgson <olly@thinkdrastic.net>
2012-11-27 16:26:35 +0000

    Added WAI-ARIA labels and states
    The expanding divs get aria-hidden and aria-expanded states at the
    appropriate times, along with an aria-labelledby attribute (pointing at
    the link which toggles them). In turn, the link gets an id and an
    aria-controls attribute (pointing at the div it toggles). I've also
    started adding the relevant role atttibutes. I'm using
    http://www.w3.org/TR/wai-aria-practices/#accordion as a guide; I haven't
    yet got to keyboard navigation.

--- ohAccordion.jquery.js
+++ ohAccordion.jquery.js
@@ -19,7 +19,9 @@
       open by default, unless specced in the URI.
     * The script will automatically add IDs to the divs if they
       don't already have them, e.g. #expander01/02/03/etc.
-    * When open, the <h3> elements gain class="open"
+    * When open, the <h3> elements gain class="open".
+    * The script adds appropriate aria-* roles to the relevant
+      elements at the relevant moments.
 
     It's best to style it so it looks good even if the nodes are
     open, just in case javascript is unavailable for some reason.
@@ -27,34 +29,43 @@
 (function( $ ) {
 
     var methods = {
+
         /*  Init is fired on page load */
         init : function() {
 
             this.each(function(i,elm) {
 
-                $(elm).addClass("accordion-js");
+                $(elm).addClass("accordion-js").attr("role", "tablist");
 
                 /*  Cache this so we don't need to look it up over and over again */
-                var winLocHash = window.location.hash;
+                var winLocHash, parentClosed;
+                winLocHash = window.location.hash;
+
+                /*  If div.accordion is also div.closed, close everything! */
+                if ($(elm).hasClass("closed")) {
+                    parentClosed = true;
+                } else {
+                    parentClosed = false;
+                }
 
                 $(elm).children("h3").each(function (j) {
-                    var relatedDiv, divId, aHref, parentClosed;
+                    var relatedDiv, divId, aHref, aID;
 
                     relatedDiv = $(this).next();
-                    divId = relatedDiv.attr("id")
-                    /*  Give the div an ID! */
+                    divId = relatedDiv.attr("id");
+                    /*  Give the div an ID attribute (if it doesn't have one already) */
                     if (divId === undefined || divId === '') {
                         divId = "expander" + i + j;
-                        relatedDiv.attr("id", divId);
                     }
                     aHref = "#" + divId;
 
-                    /*  If div.accordion is div.closed, close everything! */
-                    if ($(elm).hasClass("closed")) {
-                        parentClosed = true;
-                    } else {
-                        parentClosed = false;
-                    }
+                    /* Set the WAI-ARIA labelled attribute on the div */
+                    aID = "toggler" + i + j;
+                    relatedDiv.attr({
+                        "id": divId,
+                        "aria-labelledby": aID,
+                        "role": "tabpanel"
+                    });
 
                     /*  Figure out which node to open
                         1. See if there's a bookmark to open, using winLocHash
@@ -68,10 +79,11 @@
                     /*  Open the bookmarked/first node accordingly, close the others */
                     if (aHref === winLocHash) {
                         $(this).addClass("open");
-                        $(this).wrapInner("<a href=" + aHref + " title='Close this section'></a>");
+                        $(this).wrapInner("<a id='" + aID + "' role='tab' href='" + aHref + "' aria-controls='" + divId + "' title='Close this section'></a>");
+                        relatedDiv.ohAccordion("toggleLeaf", "show", 300);
                     } else {
-                        $(this).wrapInner("<a href=" + aHref + " title='Expand this section'></a>");
-                        relatedDiv.hide();
+                        $(this).wrapInner("<a id='" + aID + "' role='tab' href='" + aHref + "' aria-controls='" + divId + "' title='Expand this section'></a>");
+                        relatedDiv.ohAccordion("toggleLeaf", "hide", 300);
                     }
                     /*  Add the click event to the links */
                     $(this).children("a").click(function () {
@@ -84,17 +96,33 @@
         /*  This is fired by clicking on the titles */
         toggle : function() {
             if (this.hasClass("open")) {
-                this.removeClass("open").next().hide(300);
+                this.removeClass("open").next().ohAccordion("toggleLeaf", "hide", 300);
                 this.children("a").attr("title", "Expand this section");
             } else {
-                this.parent().children("div").not(this.next()).hide(300);
-                this.next().show(300);
+                this.parent().children("div").not(this.next()).ohAccordion("toggleLeaf", "hide", 300);
+                this.next().show(300).ohAccordion("toggleLeaf", "show", 300);
                 this.parent().children("h3").not(this).removeClass("open");
                 this.addClass("open");
                 this.parent().children("h3").not(this).children("a").attr("title", "Expand this section");
                 this.children("a").attr("title", "Close this section");
             }
-            this.children("a").blur();
+            //this.children("a").blur();
+        },
+        /* Pass in one of the div elements, and a state and this will show/hide it and set the aria-expanded and aria-hidden attributes accordingly */
+        toggleLeaf : function (state, transitionspeed) {
+            if (state === "show") {
+                this.show(transitionspeed)
+                    .attr({
+                        "aria-expanded": "true",
+                        "aria-hidden": "false"
+                    });
+            } else if (state === "hide") {
+                this.hide(transitionspeed)
+                    .attr({
+                    "aria-expanded": "false",
+                    "aria-hidden": "true"
+                });
+            }
         }
     };
 
--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
 Olly's very simple accordion plugin
 ===================================
 
-To use, make content that looks like this:
+    To use, make content that looks like this:
 
-    <div class="accordion">
+    <div class="accordion [closed]">
         <h3>Section title</h3>
         <div>Section content</div>
         <h3>Section title</h3>
@@ -11,12 +11,13 @@ To use, make content that looks like this:
         [etc]
     </div>
 
-Then use ```$("div.accordion").ohAccordion()``` in ```document.ready()```.
+    Then use $("div.accordion").ohAccordion() in document.ready().
 
-* By default, first branch is opened.
-* Supports bookmarking, e.g. ```this.aspx#expander01```
-* A class of closed on the top div means no branches are open by default (unless specified in the URI), e.g. ```<div class="accordion closed"```>
-* The script will automatically add ID attributes to the divs if they don't already have them, e.g. ```#expander01```.
-* When open, the ```<h3>``` elements gain ```class="open"```
+    * By default, first branch is opened.
+    * Supports bookmarking, e.g. this.aspx#expander01
+    * A class of closed on the top div means no branches are open by default, unless specced in the URI.
+    * The script will automatically add IDs to the divs if they don't already have them, e.g. #expander01/02/03/etc.
+    * When open, the <h3> elements gain class="open".
+    * The script adds appropriate aria-* roles to the relevant elements at the relevant moments.
 
-It's best to style it so it looks good even if the nodes are open, just in case javascript is unavailable for some reason.
+    It's best to style it so it looks good even if the nodes are open, just in case javascript is unavailable for some reason.
